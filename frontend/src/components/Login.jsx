f05078c984810a2aa72690d970abc8a4346fbb9e
KJH <carune99@gmail.com>
2025-03-20 22:58:10 +0900

    로그인 화면 수정

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,39 +1,23 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./Login.scss";
+import loginImage from "../assets/gureum1.gif";
 
 const Login = () => {
   const [isSignUp, setIsSignUp] = useState(false);
-
-  useEffect(() => {
-    const signupButton = document.getElementById("sign-up");
-    const signinButton = document.getElementById("sign-in");
-
-    if (signupButton && signinButton) {
-      signupButton.addEventListener("click", () => setIsSignUp(true));
-      signinButton.addEventListener("click", () => setIsSignUp(false));
-    }
-
-    return () => {
-      if (signupButton && signinButton) {
-        signupButton.removeEventListener("click", () => setIsSignUp(true));
-        signinButton.removeEventListener("click", () => setIsSignUp(false));
-      }
-    };
-  }, []);
 
   return (
     <div className="login">
       <div className="login__content">
         <div className="login__img">
           <img
-            src="https://image.freepik.com/free-vector/code-typing-concept-illustration_114360-3581.jpg"
-            alt="user login"
+            src={loginImage}
+            alt="구름월드 로그인"
           />
         </div>
         <div className="login__forms">
           {/* 로그인 폼 */}
-          <form action="" className={`login__register ${!isSignUp ? "block" : "none"}`} id="login-in">
-            <h1 className="login__title">Sign In</h1>
+          <div className={`login__register ${!isSignUp ? "block" : "none"}`}>
+            <h1 className="login__title">구름월드 입장</h1>
             <div className="login__box">
               <i className="bx bx-user login__icon"></i>
               <input type="text" placeholder="Username" className="login__input" />
@@ -43,20 +27,26 @@ const Login = () => {
               <input type="password" placeholder="Password" className="login__input" />
             </div>
             <a href="#" className="login__forgot">
-              Forgot Password?
+              비밀번호를 잊으셨나요?
             </a>
-            <a href="#" className="login__button">Sign In</a>
+            <button className="login__button">로그인</button>
             <div>
-              <span className="login__account login__account--account">Don't Have an Account?</span>
-              <span className="login__signin login__signin--signup" id="sign-up">
-                Sign Up
+              <span className="login__account">계정이 없으신가요? </span>
+              <span className="login__signin login__signin--signup" id="sign-up" onClick={() => setIsSignUp(true)}>
+                회원가입
               </span>
             </div>
-          </form>
+            <div className="login__social">
+              <a href="#" className="login__social--icon"><i className="bx bxl-facebook"></i></a>
+              <a href="#" className="login__social--icon"><i className="bx bxl-twitter"></i></a>
+              <a href="#" className="login__social--icon"><i className="bx bxl-google"></i></a>
+              <a href="#" className="login__social--icon"><i className="bx bxl-github"></i></a>
+            </div>
+          </div>
 
           {/* 회원가입 폼 */}
-          <form action="" className={`login__create ${isSignUp ? "block" : "none"}`} id="login-up">
-            <h1 className="login__title">Create Account</h1>
+          <div className={`login__create ${isSignUp ? "block" : "none"}`}>
+            <h1 className="login__title">회원가입</h1>
             <div className="login__box">
               <i className="bx bx-user login__icon"></i>
               <input type="text" placeholder="Username" className="login__input" />
@@ -69,20 +59,14 @@ const Login = () => {
               <i className="bx bx-lock login__icon"></i>
               <input type="password" placeholder="Password" className="login__input" />
             </div>
-            <a href="#" className="login__button">Sign Up</a>
+            <button className="login__button">가입완료</button>
             <div>
-              <span className="login__account login__account--account">Already have an Account?</span>
-              <span className="login__signup login__signup--signup" id="sign-in">
-                Sign In
+              <span className="login__account">이미 계정이 있으신가요? </span>
+              <span className="login__signup login__signup--signup" id="sign-in" onClick={() => setIsSignUp(false)}>
+                로그인
               </span>
             </div>
-            <div className="login__social">
-              <a href="#" className="login__social--icon"><i className="bx bxl-facebook"></i></a>
-              <a href="#" className="login__social--icon"><i className="bx bxl-twitter"></i></a>
-              <a href="#" className="login__social--icon"><i className="bx bxl-google"></i></a>
-              <a href="#" className="login__social--icon"><i className="bx bxl-github"></i></a>
-            </div>
-          </form>
+          </div>
         </div>
       </div>
     </div>
